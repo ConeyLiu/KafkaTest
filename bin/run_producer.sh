d5b98e5fec2c2c7e08b29b4d6421b18c12b6781f
Xianyang Liu <xianyang.liu@intel.com>
2017-06-26 16:49:28 +0800

    small fix

--- bin/run_producer.sh
+++ bin/run_producer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 current_dir = dirname $0
-java -Xms 2G -Xmx 2G -cp ${current_path}../target/kafkaTest-1.0-SNAPSHOT.jar com.intel.ProducerPerformance $@
+java -Xms 2G -Xmx 2G -cp ${current_path}/../target/kafkaTest-1.0-SNAPSHOT.jar com.intel.ProducerPerformance $@
