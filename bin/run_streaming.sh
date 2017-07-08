33d98f6edc21aeb9407108738fce6bb67a699fa1
Xianyang Liu <xianyang.liu@intel.com>
2017-07-08 22:32:59 +0800

    streaming update

--- bin/run_streaming.sh
+++ bin/run_streaming.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR=$(dirname "$0")
-
-{SPARK_HOME}bin/spark-submit \
+SPARK_HOME=/home/xianyang/opt/spark
+${SPARK_HOME}/bin/spark-submit \
           --class com.intel.Main               \
-          --master         spark://master7077  \
+          --master         yarn                \
           --deploy-mode    client              \
+          --executor-memory 30G               \
+          --executor-cores  10                \
+          --num-executors 3                    \
           ${CURRENT_DIR}/../target/kafkaTest-1.0-SNAPSHOT.jar   \
           $@
 
--- src/main/scala/com/intel/Main.scala
+++ src/main/scala/com/intel/Main.scala
@@ -1,20 +1,19 @@
 package com.intel
 
 import java.io.IOException
-import java.util.{Properties, Random}
+import java.util.{Collections, Properties, Random}
 import java.lang.{Boolean => jBoolean}
 
 import com.codahale.metrics.{Counter, Histogram}
 import joptsimple.OptionParser
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.TopicPartition
 
 import scala.collection.JavaConverters._
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-
-import scala.collection.mutable
 
 
 object Main {
@@ -24,8 +23,7 @@ object Main {
     var config: StreamingConfig = null
     var rpsHistogram: Histogram = null
     var mpsHistogram: Histogram = null
-    var count: Counter = null
-    var occurError = true
+    var counter: Counter = null
     val start = System.currentTimeMillis()
     try {
 
@@ -40,7 +38,7 @@ object Main {
       val metrics = MetricsUtil.getMetrics
       rpsHistogram = MetricsUtil.getHistogram("streaming_record_per_second_" + topic, metrics)
       mpsHistogram = MetricsUtil.getHistogram("streaming_mb_per_second_" + topic, metrics)
-      count = MetricsUtil.getCounter("streaming_count_" + topic, metrics)
+      counter = MetricsUtil.getCounter("streaming_count_" + topic, metrics)
 
       val kafkaData = KafkaUtils.createDirectStream[Array[Byte], Array[Byte]](
         ssc,
@@ -50,58 +48,10 @@ object Main {
 
       if (!config.randomRead) {
         kafkaData.foreachRDD { rdd =>
-          val first = rdd.first()
-          val sizePerRecord = {
-            var count = 0
-            if (first.key() != null) {
-              count += first.key().length
-            }
-            if (first.value() != null) {
-              count += first.value().length
-            }
-            count
-          }
-
-          val count = rdd.count()
-          val mbRead = (count * sizePerRecord) / (1024 * 1024)
-          rpsHistogram.update(count / config.batchInterval.toInt)
-          mpsHistogram.update(mbRead / config.batchInterval.toInt)
-        }
-      } else {
-        // map: partitionID -> maxEndOffSet
-        val offsetMap = new mutable.HashMap[Int, Long]()
-        kafkaData.foreachRDD { rdd =>
-          val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
-          if (offsetMap.nonEmpty) {
-            val random = new Random()
-            val randomOffset = rdd.partitions.map { p =>
-              // update the offset
-              val offsetRange = offsetRanges(p.index)
-              if (offsetRange.untilOffset > offsetMap.getOrElse(p.index, 0L)) {
-                offsetMap.put(p.index, offsetRange.untilOffset)
-              }
-
-              val endOffset = random.nextInt(offsetMap.getOrElse(p.index, 0L).toInt)
-              val startOffset = if ((endOffset - 2000) >= 0) {
-                endOffset - 2000
-              } else {
-                0
-              }
-              OffsetRange(topic, p.index, startOffset, endOffset)
-            }
-
-            val randomRdd = KafkaUtils.createRDD[Array[Byte], Array[Byte]](
-              ssc.sparkContext,
-              config.props.asJava,
-              randomOffset,
-              LocationStrategies.PreferConsistent
-            ).map(record => (record.key(), record.value()))
-
-            val kvRdd = rdd.map(record => (record.key(), record.value()))
-            val unionRDD = randomRdd.union(kvRdd)
-            val count = unionRDD.count()
-
-            val first = unionRDD.first()
+          val kvRdd = rdd.map(cr => (cr.key(), cr.value()))
+          val count = kvRdd.count()
+          if (count != 0) {
+            val first = kvRdd.first()
             val sizePerRecord = {
               var count = 0
               if (first._1 != null) {
@@ -113,6 +63,64 @@ object Main {
               count
             }
 
+            counter.inc(count)
+            val mbRead = (count * sizePerRecord) / (1024 * 1024)
+            rpsHistogram.update(count / config.batchInterval.toInt)
+            mpsHistogram.update(mbRead / config.batchInterval.toInt)
+          }
+
+        }
+      } else {
+        // create a consumer just used for poll the offset range.
+        val consumer = new KafkaConsumer[Array[Byte], Array[Byte]](config.props.asJava)
+        consumer.subscribe(Collections.singletonList(config.topic))
+        val partitionInfos = consumer.partitionsFor(config.topic).asScala
+        val topicPartitions = partitionInfos.map(pi => new TopicPartition(config.topic, pi.partition()))
+        // because this test just for read data (already store in Kafka), so the beginning offset and end offset
+        // are fixed.
+        val beginningOffsets = consumer.beginningOffsets(topicPartitions.asJava)
+
+        kafkaData.foreachRDD { rdd =>
+          val endOffsets = consumer.endOffsets(topicPartitions.asJava)
+          val random = new Random(System.currentTimeMillis())
+          val randomOffset = topicPartitions.map { tp =>
+            val beginningOffset = beginningOffsets.get(tp)
+            val endOffset = endOffsets.get(tp)
+            val seekEndPosition = beginningOffset + random.nextInt((endOffset - beginningOffset).toInt).toLong
+            val seekStartPosition = if ((seekEndPosition - 2000) >= beginningOffset ) {
+              seekEndPosition - 2000L
+            } else {
+              beginningOffset
+            }.toLong
+
+            OffsetRange(tp, seekStartPosition, seekEndPosition)
+          }
+
+          val randomRdd = KafkaUtils.createRDD[Array[Byte], Array[Byte]](
+            ssc.sparkContext,
+            config.props.asJava,
+            randomOffset.toArray,
+            LocationStrategies.PreferConsistent
+          ).map(record => (record.key(), record.value()))
+
+          val kvRdd = rdd.map(record => (record.key(), record.value()))
+          val unionRDD = randomRdd.union(kvRdd)
+          val count = unionRDD.count()
+
+          if (count != 0) {
+            val first = kvRdd.first()
+            val sizePerRecord = {
+              var count = 0
+              if (first._1 != null) {
+                count += first._1.length
+              }
+              if (first._2 != null) {
+                count += first._2.length
+              }
+              count
+            }
+
+            counter.inc(count)
             val mbRead = (count * sizePerRecord) / (1024 * 1024)
             rpsHistogram.update(count / config.batchInterval.toInt)
             mpsHistogram.update(mbRead / config.batchInterval.toInt)
@@ -122,8 +130,7 @@ object Main {
 
 
       ssc.start()
-      occurError = false
-      ssc.awaitTermination()
+      ssc.awaitTerminationOrTimeout(config.runningTime)
 
       println("Test finished!")
     } catch {
@@ -137,14 +144,14 @@ object Main {
         System.exit(-2)
       }
     } finally {
-      if (config != null && rpsHistogram != null && mpsHistogram != null && count != null) {
+      if (config != null && rpsHistogram != null && mpsHistogram != null && counter != null) {
         MetricsUtil.reportStream(
           System.currentTimeMillis() - start,
           config.outputDir,
           "streaming" + config.whetherRandom,
           rpsHistogram,
           mpsHistogram,
-          count)
+          counter)
       }
     }
 	}
@@ -163,6 +170,10 @@ object Main {
       .withRequiredArg
       .describedAs("topic")
       .ofType(classOf[String])
+    val runningTimeOpt = parser.accepts("runningTime", "REQUIRED: The total running time for test.")
+      .withRequiredArg()
+      .describedAs("runningTime")
+      .ofType(classOf[Long])
     val groupIdOpt = parser.accepts("group", "The group id to consume on.")
       .withRequiredArg
       .describedAs("gid")
@@ -171,8 +182,8 @@ object Main {
     val randomReadOpt = parser.accepts("randomRead", "Whether random read the data.")
       .withRequiredArg()
       .describedAs("randomRead")
-      .defaultsTo("false")
       .ofType(classOf[Boolean])
+      .defaultsTo(false)
     val outputDirOpt = parser.accepts("outputDir", "REQUIRED: The report output dir.")
       .withRequiredArg()
       .describedAs("outputDir")
@@ -182,20 +193,14 @@ object Main {
       .describedAs("size")
       .ofType(classOf[java.lang.Integer])
       .defaultsTo(1024 * 1024)
-    val socketBufferSizeOpt = parser.accepts("socket-buffer-size", "The size of the tcp RECV size.")
-      .withRequiredArg
-      .describedAs("size")
-      .ofType(classOf[java.lang.Integer])
-      .defaultsTo(2 * 1024 * 1024)
 
     val options = parser.parse(args: _*)
-
-
 
     val randomRead = options.valueOf(randomReadOpt).booleanValue()
     val batchInterval = options.valueOf(batchIntervalOpt)
     val topic = options.valueOf(topicOpt)
     val outputDir = options.valueOf(outputDirOpt).toString
+    val runningTime = options.valueOf(runningTimeOpt).toLong
 
     val whetherRandom = if (randomRead) {
       "_random"
@@ -206,12 +211,12 @@ object Main {
     val props = Map[String, Object](
       ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> options.valueOf(bootstrapServersOpt).toString,
       ConsumerConfig.GROUP_ID_CONFIG -> (options.valueOf(groupIdOpt) + whetherRandom),
-      ConsumerConfig.RECEIVE_BUFFER_CONFIG -> options.valueOf(socketBufferSizeOpt).toString,
       ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG -> options.valueOf(fetchSizeOpt).toString,
       ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[ByteArrayDeserializer],
       ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[ByteArrayDeserializer],
       ConsumerConfig.CHECK_CRCS_CONFIG -> (false: jBoolean),
-      ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (false: jBoolean)
+      ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (false: jBoolean),
+      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "latest"
     )
   }
 }
